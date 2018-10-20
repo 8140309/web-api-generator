460d217dd668b2fab9ff5208949b7dd62cbe19e3
Fernando Sousa <8140309@estg.ipp.pt>
2018-10-20 15:19:49 +0100

    oauth

--- config.js
+++ config.js
@@ -4,8 +4,11 @@ var config = {};
 config.appName = "nome-do-repo11";
 config.homepage = "https://localhost:5000"
 
-config.clientId = "b501db557e6dc37d83e9";
-config.clientSecret = "440b8d91212a2d78ca43ca2862e30e5b8898a5cb";
+//config.clientId = "b501db557e6dc37d83e9";
+//config.clientSecret = "440b8d91212a2d78ca43ca2862e30e5b8898a5cb";
+
+config.clientId = "db22c7f8720dbb67ea3b";
+config.clientSecret = "e9dfaa5b2f068c7f614cbcba33242ff792904814";
 
 config.defaultConnectionString = "mongodb+srv://fernando:654321aA@cluster0-sejql.gcp.mongodb.net/movies?retryWrites=true"
 config.port = 8080;
